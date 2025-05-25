443a1e10cf6eb456590faac0287287dacd139938
PhosFactum <phosphorus.work@gmail.com>
2025-05-25 19:30:16 +0300

    feat(migrations)

--- run.sh
+++ run.sh
@@ -3,6 +3,10 @@
 # Activating venv
 source .venv/bin/activate
 
+# Migrations of Alembic
+echo "Running Alembic migrations..."
+alembic upgrade head
+
 # Installing PYTHONPATH on ./src for uvicorn
 export PYTHONPATH="./src"
 uvicorn main:app --reload --host 0.0.0.0 --port 8000
